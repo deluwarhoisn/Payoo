1f3a296c7acfef7cf65da9ce42ce8b82ae7ff30f
deluwarhoisn <babuhossen301@gmail.com>
2025-08-23 02:02:14 +0600

    re cap form

--- assets/home.js
+++ assets/home.js
@@ -1,15 +1,28 @@
-document.getElementById("add-money-btn").addEventListener("click",function(e){
-    e.preventDefault();
-   console.log('bank all form')
-    const bank = document.getElementById("bank").value
-    console.log(bank)
-    const  accountNumber = document.getElementById('account-number').value 
-    console.log(accountNumber)
-    const amount = parseInt(document.getElementById('add-amount').value)
-    const pin = document.getElementById('add-pin').value
-   const AvailableBalence = parseInt(document.getElementById('Available-balence').innerText)
+// document.getElementById("add-money-btn").addEventListener("click",function(e){
+//     e.preventDefault();
+//    console.log('bank all form')
+//     const bank = document.getElementById("bank").value
+//     console.log(bank)
+//     const  accountNumber = document.getElementById('account-number').value 
+//     console.log(accountNumber)
+//     const amount = parseInt(document.getElementById('add-amount').value)
+//     const pin = document.getElementById('add-pin').value
+//    const AvailableBalence = parseInt(document.getElementById('Available-balence').innerText)
 
- console.log(AvailableBalence)
+//  console.log(AvailableBalence)
+//     const totalNewAvailableBalance = amount + AvailableBalence
+//     document.getElementById('Available-balence').innerText = totalNewAvailableBalance
+// })
+
+document.getElementById('add-money-btn').addEventListener('click',function(e){
+    e.preventDefault()
+    const  bank = document.getElementById('bank').value 
+    const accountNumber = document.getElementById('account-number').value
+    const amount = parseInt(document.getElementById('add-amount').value )
+    const pinNumber = document.getElementById('add-pin').value
+    const AvailableBalence = parseInt(document.getElementById('Available-balence').innerText)
+    console.log(amount,AvailableBalence)
     const totalNewAvailableBalance = amount + AvailableBalence
-    document.getElementById('Available-balence').innerText = totalNewAvailableBalance
+
+    document.getElementById('Available-balence').innerText=totalNewAvailableBalance
 })
