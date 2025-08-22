3c3a99ac48a1fc6aeb5169e848c7d04af4766686
deluwarhoisn <babuhossen301@gmail.com>
2025-08-22 23:28:12 +0600

    step -2

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+
+ document.getElementById("LoginButton").addEventListener("click", function(event) {
+  event.preventDefault();
+
+  let mobileNumber = 12345678910;
+  let pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const pinNumberValue = document.getElementById("pin-number").value;
+
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+  if (mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber) {
+    window.location.href = "./home.html";
+  } else {
+    alert("no pages");
+  }
+});
+
+
+
+
+// document.getElementById("LoginButton").addEventListener("click",function(event){
+// event.preventDefault();
+// const mobileNumber =12345678910
+// const pinNumber = 1234
+
+// const mobileNumberValue = document.getElementById("mobile-number").value;
+// const mobileNumberValueConverted = parseInt(mobileNumberValue)
+// const pinNumberValue = document.getElementById("pin-number").value;
+// const pinNumberValueConverted = parseInt(pinNumberValue)
+
+
+// if(mobileNumberValueConverted ===  mobileNumber && pinNumberValueConverted === pinNumber){
+//   window.location.href="./home.html"
+// }
+// else{
+//     alert('no pages')
+// }
+// })
