0fdd8d7d282f9f6cd0b9b860cc815e7a9de3ba08
deluwarhoisn <babuhossen301@gmail.com>
2025-08-23 22:34:30 +0600

    alomalo code

--- assets/home.js
+++ assets/home.js
@@ -13,15 +13,18 @@
 //     const totalNewAvailableBalance = amount + AvailableBalence
 //     document.getElementById('Available-balence').innerText = totalNewAvailableBalance
 // })
-const validPin =1234
+// const validPin =1234
 document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault()
     const  bank = document.getElementById('bank').value 
     const accountNumber = document.getElementById('account-number').value
     const amount = parseInt(document.getElementById('add-amount').value )
-    const pinNumber = parent(document.getElementById('add-pin').value)
-    const AvailableBalence = parseInt(document.getElementById('Available-balence').innerText)
-    console.log(amount,AvailableBalence)
+    const pinNumber = parseInt(document.getElementById('add-pin').value)
+
+    const AvailableBalance = parseInt(document.getElementById('Available-balance').innerText)
+
+  const totalNewAvailableBalance = amount + AvailableBalance
+
 
     if(accountNumber.length <11){
         alert('please provide valid account number')
@@ -32,7 +35,24 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
         alert('please provid valid pin')
         return;
     }
-    const totalNewAvailableBalance = amount + AvailableBalence
+   
+  
+  document.getElementById('Available-balance').innerText=totalNewAvailableBalance
+    document.getElementById('Available-balance').innerText=totalNewAvailableBalance
 
-    document.getElementById('Available-balence').innerText=totalNewAvailableBalance
+})
+
+// toggling feuter
+
+document.getElementById('add-money-btn').addEventListener('click',function(){
+    document.getElementById('add-money-parent').style.display='block'
+    
+    document.getElementById('cash-out-parent').style.display='none'
+})
+
+document.getElementById('cash-out-button').addEventListener('click',function(){
+   document.getElementById('cash-out-parent').style.display='block'
+    document.getElementById('add-money-parent').style.display='none'
+    
+ 
 })
