99a78485c498908a4d27e624486a062f78d3db95
deluwarhoisn <babuhossen301@gmail.com>
2025-08-23 11:42:31 +0600

    bton-click -none toggle

--- assets/home.js
+++ assets/home.js
@@ -13,15 +13,25 @@
 //     const totalNewAvailableBalance = amount + AvailableBalence
 //     document.getElementById('Available-balence').innerText = totalNewAvailableBalance
 // })
-
+const validPin =1234
 document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault()
     const  bank = document.getElementById('bank').value 
     const accountNumber = document.getElementById('account-number').value
     const amount = parseInt(document.getElementById('add-amount').value )
-    const pinNumber = document.getElementById('add-pin').value
+    const pinNumber = parent(document.getElementById('add-pin').value)
     const AvailableBalence = parseInt(document.getElementById('Available-balence').innerText)
     console.log(amount,AvailableBalence)
+
+    if(accountNumber.length <11){
+        alert('please provide valid account number')
+        return;
+    }
+
+    if(pin !== validPin){
+        alert('please provid valid pin')
+        return;
+    }
     const totalNewAvailableBalance = amount + AvailableBalence
 
     document.getElementById('Available-balence').innerText=totalNewAvailableBalance
